8a833a729a5c8d696d1ec8953aa80a92fce00f3f
Tobias Weber <toby-weber@gmx.de>
2025-01-10 14:31:42 +0100

    add declaration of BladeGeoData Object with atribute vectors like chord, radius etc

--- BladeGeoData.h
+++ BladeGeoData.h
@@ -1,5 +1,19 @@
-#pragma once
-class BladeGeoData
-{
+# pragma once
+
+#include <vector>
+
+using namespace std;
+
+class BladeGeoData {
+public:
+	vector<double> bladeRadius;
+	vector<double> bladeChord;
+	vector<double> bladeRelativeThickness;
+	vector<double> bladeTwist;
+	vector<double> bladeXt4Position;
+	vector<double> bladeYt4Position;
+	vector<double> bladePCBAX;
+	vector<double> bladePCBAY;
+	vector<double> bladeRelativeTwistAxis;
 };
 
