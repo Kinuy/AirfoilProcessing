c11ad6983eeddeecfa21ef055471994df256cc7f
Tobias Weber <toby-weber@gmx.de>
2025-01-10 14:38:30 +0100

    add project data object to store project relevant data in like airfoil blade sections airfoil data and blade geo data.

--- ProjectData.h
+++ ProjectData.h
@@ -1,5 +1,19 @@
-#pragma once
-class ProjectData
-{
-};
+# pragma once
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class ProjectData {
+public:
+	bool takeAnalyseSectionsInput;
+	double hubRadius;
+	double bladeLength;
+	string airfoilGeoSummaryFileName;
+	string airfoilPerfoSummaryFileName;
+	string bladeGeoFileName;
+	vector<double> bladeAnalyseSections;
+	vector<double> bladeSegmentWidth;;
+	vector<double> relThicknessAtAnalyseSections;
+};
