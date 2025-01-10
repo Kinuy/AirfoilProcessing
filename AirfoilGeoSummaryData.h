cf92a33889c0870f278ad6c41dd9236982c321cb
Tobias Weber <toby-weber@gmx.de>
2025-01-10 14:33:27 +0100

    add declaration of object of airfoil geo data to store raw and interpolated airfoil geo objects in vectos

--- AirfoilGeoSummaryData.h
+++ AirfoilGeoSummaryData.h
@@ -1,5 +1,14 @@
-#pragma once
-class AirfoilGeoSummaryData
-{
-};
+# pragma once
 
+#include <vector>
+
+#include "AirfoilGeoData.h"
+
+using namespace std;
+
+class AirfoilGeoSummaryData {
+public:
+	vector<string> AirfoilGeoDataFileNameList;
+	vector<AirfoilGeoData*> AirfoilGeoDataList;
+	vector<AirfoilGeoData*> AirfoilInterpolatedGeoDataList;
+};
