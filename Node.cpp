ac31c3a9f3e92971a5d52ea5046784d67d57d649
Tobias Weber <toby-weber@gmx.de>
2025-01-10 14:35:34 +0100

    add node class to make an object with metadata of each airfoil 2d point

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,5 @@
 #include "Node.h"
+
+Node::Node(int index, double x, double y, bool isTop, bool isLastFlatbackPoint) : mIndex(index), mX(x), mY(y), mIsTop(isTop), mIsLastFlatbackPoint(isLastFlatbackPoint)
+{
+}
--- Node.h
+++ Node.h
@@ -1,5 +1,20 @@
-#pragma once
-class Node
-{
-};
+# pragma once
 
+class Node {
+public:
+	Node(int index, double x, double y, bool isTop, bool isLastFlatbackPoint);
+	double X() { return mX; }
+	double Y() { return mY; }
+	bool IsTop() { return mIsTop; }
+	bool IsLastFlatbackPoint() { return mIsLastFlatbackPoint; }
+	int Index() { return mIndex; }
+private:
+	int mIndex;
+	double mX;
+	double mY;
+	bool mIsLastFlatbackPoint;
+	bool mIsTop;
+	bool mTopBottom;
+	enum mSide { ps, ss };
+
+};
